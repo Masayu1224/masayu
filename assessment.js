996cbb64050e3c21daadcc5cffd05ab2b5986749
Masayu1224 <59610760+Masayu1224@users.noreply.github.com>
2020-09-15 18:11:49 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -4,7 +4,7 @@
   const assessmentButton = document.getElementById('assessment');
   const resultDivided = document.getElementById('result-area');
   const tweetDivided = document.getElementById('tweet-area');
-  
+
   /**
   * 指定した要素の子どもをすべて除去する
   * @param {HTMLElement} element HTMLの要素
@@ -14,37 +14,37 @@
           element.removeChild(element.firstChild);
       }
   }
-  
+
   assessmentButton.onclick = () => {
       const userName = userNameInput.value;
       if (userName.length === 0) { // 名前が空の時は処理を終了する
           return;
       }
-      
+
       // 診断結果表示エリアの作成
       removeAllChildren(resultDivided);
       const header = document.createElement('h3');
       header.innerText = '診断結果';
       resultDivided.appendChild(header);
-      
+
       const paragraph = document.createElement('p');
       const result = assessment(userName);
       paragraph.innerText = result;
       resultDivided.appendChild(paragraph);
-      
+
       // ツイートエリアの作成
       removeAllChildren(tweetDivided);
       const anchor = document.createElement('a');
       const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D&text='
       + encodeURIComponent(result);
       anchor.setAttribute('href', hrefValue);
-      anchor.className  'twitter-hashtag-button';
+      anchor.className = 'twitter-hashtag-button';
       anchor.innerText = 'Tweet #%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D';
       tweetDivided.appendChild(anchor);
-      
+
       twttr.widgets.load();
   };
-  
+
   const answers = [
       '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
       '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
@@ -63,7 +63,7 @@
       '{userName}のいいところはその全てです。{userName}自身がいいところなのです。',
       '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。',
   ];
-  
+
   /**
   * 名前の文字列を渡すと診断結果を返す関数
   * @param {string] userName ユーザーの名前
@@ -75,21 +75,22 @@
       for (let i = 0; i < userNamelength; i++) {
           sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
       }
-      
+
       // 文字のコード番号の合計を回答の数で割って添字の数値を求める
       const index = sumOfcharCode % answers.length;
       let result = answers[index];
-      
+
       result = result.replace(/{userName]/g, userName);
       return result;
   }
-  
+
   // テストコード
   console.assert(
       assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
       '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
   );
-  console.assert('太郎') === assessment('太郎'),
+  console.assert(
+  assessment('太郎') === assessment('太郎'),
   '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
   );
 })();
